ac5e6c0517e568941b90f58164f790187718d96f
Theodore Baschak <theodore@ciscodude.net>
2016-01-10 22:51:06 -0600

    spoof packet twice, once from 1.1.1.1 once from RandIP()

--- bcp38-client-spoof.py
+++ bcp38-client-spoof.py
@@ -13,13 +13,16 @@ if r.status_code == 200:
 else:
     ipv4 = 'err'
 
-#print ipv4
-
 message = "This is a test message from\n%s" % ipv4
 
-server	= '192.168.203.22'
+server	= '192.168.202.33'
 fakesrc = '1.1.1.1'
 
 mypacket = IP(dst=server, src=fakesrc)/UDP(sport=RandShort(),dport=10000)/message
 l2packet = Ether()/mypacket
 sendp(l2packet)
+
+mypacket = IP(dst=server, src=RandIP())/UDP(sport=RandShort(),dport=10000)/message
+l2packet = Ether()/mypacket
+sendp(l2packet)
+
