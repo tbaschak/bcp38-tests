1bc26126760eec54f6890ad63561c075beb00130
Theodore Baschak <theodore@ciscodude.net>
2016-01-10 22:31:58 -0600

    fix syntax error

--- bcp38-client-spoof.py
+++ bcp38-client-spoof.py
@@ -20,6 +20,6 @@ message = "This is a test message from\n%s" % ipv4
 server	= '192.168.203.22'
 fakesrc = '1.1.1.1'
 
-mypacket = IP(dst=server, src=fakesrc)/UDP(sport=RandShort(),dport=10000)/message)
+mypacket = IP(dst=server, src=fakesrc)/UDP(sport=RandShort(),dport=10000)/message
 l2packet = Ether()/mypacket
 sendp(l2packet)
