c63a8ec49f414d046bb90f5d4fb29256158d7fae
Theodore Baschak <theodore@ciscodude.net>
2016-01-10 23:27:08 -0600

    make old version into verbose version, new one only expects IP

--- bcp38-server-verbose.py
+++ bcp38-server-verbose.py
@@ -8,18 +8,18 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
 server_address = ('0.0.0.0', 10000)
-#print >>sys.stderr, 'starting up on %s port %s' % server_address
+print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
 while True:
-#    print >>sys.stderr, '\nwaiting to receive message'
+    print >>sys.stderr, '\nwaiting to receive message'
     data, address = sock.recvfrom(4096)
     
-#    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-#    print >>sys.stderr, data
+    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    print >>sys.stderr, data
 
     responselines = data.splitlines()
-    src_ip = responselines[0]
+    src_ip = responselines[1]
     udp_ip = address[0]
 
     if src_ip is udp_ip:
